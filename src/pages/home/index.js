30285ca442599dba4cb676731bbeeb76fa07e63d
raoni157 <raoni.itar@gmail.com>
2021-10-09 21:03:23 -0300

    i love my work

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,15 +5,61 @@ import { WiMoonWaningCrescent2 } from 'react-icons/wi';
 import { FiSearch } from 'react-icons/fi';
 
 // style component
-import { Section, Header, Article, Nav } from './style';
+import { Section, Header, Article, Nav, Countries } from './style';
 
 export default class Home extends Component {
   state = {
     input: '',
+    countries: [],
   };
 
+  componentDidMount() {
+    this.returnGriDiv();
+  }
+
+  async countries() {
+    this.response = await fetch('https://restcountries.com/v2/all');
+    this.data = await this.response.json();
+    return this.data;
+  }
+
+  returnGriDiv() {
+    const arrayOfCountri = [];
+    this.countries()
+      .then((response) => {
+        for (let i = 0; i <= response.length - 1; i += 1) {
+          const div = (
+            <div className="countries-card" id={response[i].name}>
+              <div className="flag-conteiner">
+                <img src={response[i].flags.png} alt="countries flags" />
+              </div>
+              <article>
+                <h1>{response[i].name}</h1>
+                <div>
+                  <h3>
+                    Population: <span>{response[i].population}</span>
+                  </h3>
+                  <h3>
+                    Region: <span>{response[i].region}</span>
+                  </h3>
+                  <h3>
+                    Capital: <span>{response[i].capital}</span>
+                  </h3>
+                </div>
+              </article>
+            </div>
+          );
+          arrayOfCountri.push(div);
+        }
+        this.setState({
+          countries: arrayOfCountri,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
   render() {
-    const { input } = this.state;
+    const { input, countries } = this.state;
     return (
       <>
         <Section>
@@ -50,6 +96,9 @@ export default class Home extends Component {
               </div>
             </Nav>
           </Article>
+          <Countries className="countries-conteiner">
+            {countries.map((value) => value)}
+          </Countries>
         </Section>
       </>
     );
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -3,9 +3,10 @@ import styled, { css } from 'styled-components';
 // color
 import * as color from '../../config/color';
 
-function displayFlex(justifyContent, alignItens) {
+function displayFlex(justifyContent, flexDirection, alignItens) {
   return css`
     display: flex;
+    flex-direction: ${flexDirection};
     justify-content: ${justifyContent};
     align-items: ${alignItens};
   `;
@@ -17,7 +18,7 @@ export const Section = styled.section`
   background-color: ${color.VeryDarkBlue};
 `;
 export const Header = styled.header`
-  ${displayFlex('space-between', 'center')}
+  ${displayFlex('space-between', 'row', 'center')}
   width: 100%;
   padding: 1.5rem 7rem;
   background-color: ${color.DarkBlue};
@@ -26,11 +27,11 @@ export const Header = styled.header`
     color: ${color.FontColorWhite};
   }
   .header-component-two {
-    ${displayFlex('center', 'center')}
+    ${displayFlex('center', 'row', 'center')}
     width: auto;
     color: ${color.FontColorWhite};
     .moonIcon {
-      ${displayFlex('center', 'center')}
+      ${displayFlex('center', 'row', 'center')}
     }
     h2 {
       font-size: 1.2rem;
@@ -42,11 +43,11 @@ export const Article = styled.article`
   padding: 0rem 7rem;
 `;
 export const Nav = styled.nav`
-  ${displayFlex('space-between', 'center')}
+  ${displayFlex('space-between', 'row', 'center')}
   width: 100%;
-  margin-top: 3rem;
+  margin-top: 4rem;
   .search-content {
-    ${displayFlex('center', 'center')}
+    ${displayFlex('center', 'row', 'center')}
     padding: 1.3rem 1rem;
     border-radius: 5px;
     background-color: ${color.DarkBlue};
@@ -54,9 +55,9 @@ export const Nav = styled.nav`
       color: ${color.FontColorWhite};
     }
     input {
-      width: 250px;
+      width: 400px;
       margin-left: 1rem;
-      font-size: 1.2rem;
+      font-size: 1.5rem;
       color: ${color.FontColorVaryLigthGray};
       background-color: transparent;
       &::placeholder {
@@ -66,7 +67,6 @@ export const Nav = styled.nav`
   }
   .select-content {
     .custon-select {
-      margin-bottom: 1rem;
       padding: 1.3rem 1rem;
       outline: none;
       border: none;
@@ -82,6 +82,46 @@ export const Nav = styled.nav`
       }
       &:after {
         margin-top: 1rem;
+      }
+    }
+  }
+`;
+export const Countries = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  padding: 1.5rem 7rem;
+  margin-top: 4rem;
+  gap: 4rem;
+  .countries-card {
+    overflow: hidden;
+    height: 350px;
+    border-radius: 10px;
+    background-color: ${color.DarkBlue};
+    .flag-conteiner {
+      ${displayFlex('start', 'column', 'center')}
+      img {
+        width: 100%;
+        height: 200px !important;
+      }
+    }
+    article {
+      ${displayFlex('flex-start', 'column', 'start')}
+      padding: 1.7rem;
+      h1 {
+        color: ${color.FontColorWhite};
+        font-size: 2rem;
+      }
+      div {
+        ${displayFlex('flex-start', 'column', 'start')}
+        margin-top: 1rem;
+        h3 {
+          font-size: 1.4rem;
+          margin-top: 1rem;
+          color: ${color.FontColorVaryLigthGray};
+          span {
+            color: ${color.FontColorGray};
+          }
+        }
       }
     }
   }
