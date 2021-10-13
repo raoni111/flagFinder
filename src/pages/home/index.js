2a6e6e5dfe8d7f235a52331293bea5bdd44db5c6
raoni157 <raoni.itar@gmail.com>
2021-10-13 20:05:56 -0300

    implementação da aba de detalhes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,15 +3,33 @@ import React, { Component } from 'react';
 
 // icons
 import { WiMoonWaningCrescent2 } from 'react-icons/wi';
-import { FiSearch } from 'react-icons/fi';
+import { FiSearch, FiArrowLeft } from 'react-icons/fi';
 
 // style component
-import { Section, Header, Article, Nav, Countries } from './style';
-
+import {
+  Section,
+  Header,
+  Article,
+  Nav,
+  Countries,
+  CountryDetail,
+} from './style';
+// .sqi.common this.formatsLanguages()
 export default class Home extends Component {
   state = {
     input: '',
     countries: [],
+    countrieDetail: 'close-details',
+    countrie: [
+      {
+        flags: {
+          png: '',
+        },
+        currencies: [{}],
+        languages: [{}],
+      },
+    ],
+    languages: '',
   };
 
   componentDidMount() {
@@ -36,30 +54,55 @@ export default class Home extends Component {
     this.searchRegion(e.target.value.toLowerCase());
   };
 
-  buildElement = (response, index) => {
-    const div = (
-      <div className="countries-card" id={response[index].name}>
-        <div className="flag-conteiner">
-          <img src={response[index].flags.png} alt="countries flags" />
-        </div>
-        <article>
-          <h1>{response[index].name}</h1>
-          <div>
-            <h3>
-              Population: <span>{response[index].population}</span>
-            </h3>
-            <h3>
-              Region: <span>{response[index].region}</span>
-            </h3>
-            <h3>
-              Capital: <span>{response[index].capital}</span>
-            </h3>
-          </div>
-        </article>
-      </div>
-    );
-    return div;
+  openDetail = (e) => {
+    this.countrie(e.target.id)
+      .then((response) => {
+        this.setState({
+          countrieDetail: 'open-details',
+          countrie: response,
+        });
+        this.formatsLanguages(response[0].languages);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
+  closeDetail = () => {
+    this.setState({
+      countrieDetail: 'close-details',
+    });
+  };
+
+  buildElement = (response, index) => (
+    <div
+      className="countries-card"
+      id={response[index].name}
+      onClickCapture={this.openDetail}
+    >
+      <div className="flag-conteiner" id={response[index].name}>
+        <img
+          src={response[index].flags.png}
+          alt="countries flags"
+          id={response[index].name}
+        />
+      </div>
+      <article id={response[index].name}>
+        <h1 id={response[index].name}>{response[index].name}</h1>
+        <div id={response[index].name}>
+          <h3 id={response[index].name}>
+            Population: <span>{response[index].population}</span>
+          </h3>
+          <h3 id={response[index].name}>
+            Region: <span>{response[index].region}</span>
+          </h3>
+          <h3 id={response[index].name}>
+            Capital: <span>{response[index].capital}</span>
+          </h3>
+        </div>
+      </article>
+    </div>
+  );
 
   searchRegion = (param) => {
     if (param === 'all') {
@@ -67,7 +110,7 @@ export default class Home extends Component {
       return;
     }
     const arrayOfCountri = [];
-    this.countries()
+    this.allCountries()
       .then((response) => {
         for (let i = 0; i < response.length; i += 1) {
           if (response[i].region.toLowerCase() === param) {
@@ -88,7 +131,7 @@ export default class Home extends Component {
       return;
     }
     const arrayOFCountri = [];
-    this.countries()
+    this.allCountries()
       .then((response) => {
         for (let index = 0; index < response.length; index += 1) {
           const languageArray = Object.entries(response[index].translations);
@@ -114,15 +157,37 @@ export default class Home extends Component {
       });
   };
 
-  async countries() {
+  formatsLanguages = (languagesParams) => {
+    let languages = '';
+    languagesParams.map((value, index) => {
+      if (index > 0) {
+        languages += `, ${value.iso639_2}`;
+      }
+      languages += value.iso639_2;
+      return '';
+    });
+    this.setState({
+      languages,
+    });
+  };
+
+  async allCountries() {
     this.response = await fetch('https://restcountries.com/v2/all');
     this.data = await this.response.json();
     return this.data;
   }
 
+  async countrie(countrie) {
+    this.response = await fetch(
+      `https://restcountries.com/v2/name/${countrie}`,
+    );
+    this.date = await this.response.json();
+    return this.date;
+  }
+
   lookForFlag() {
     const arrayOfCountries = [];
-    this.countries()
+    this.allCountries()
       .then((response) => {
         for (let i = 0; i < response.length; i += 1) {
           arrayOfCountries.push(this.buildElement(response, i));
@@ -135,11 +200,13 @@ export default class Home extends Component {
   }
 
   render() {
-    const { input, countries } = this.state;
+    const { input, countries, countrieDetail, countrie, languages } =
+      this.state;
+    console.log(countrie[0]);
     return (
       <>
         <Section>
-          <Header>
+          <Header className={countrieDetail}>
             <h1 className="header-h header-component-one">
               Where in the World?
             </h1>
@@ -181,6 +248,74 @@ export default class Home extends Component {
               </div>
             </Nav>
           </Article>
+          <CountryDetail className={countrieDetail}>
+            <div className="back-button-conteiner">
+              <button type="button" onClick={this.closeDetail}>
+                <FiArrowLeft class="back-arrow" size="20" />
+                Back
+              </button>
+            </div>
+            <article className="conteiner-coutrie-details">
+              <div className="countrie-flag">
+                <img src={countrie[0].flags.png} alt="countrie flag" />
+              </div>
+              <div className="countrie-details">
+                <div className="common-name">
+                  <h1>{countrie[0].name}</h1>
+                </div>
+                <div className="grid-details">
+                  <div className="native-name">
+                    <h1>
+                      Native Name:
+                      <span>{countrie[0].nativeName}</span>
+                    </h1>
+                  </div>
+                  <div className="population">
+                    <h1>
+                      Population:
+                      <span>{countrie[0].population}</span>
+                    </h1>
+                  </div>
+                  <div className="region">
+                    <h1>
+                      Region:
+                      <span>{countrie[0].region}</span>
+                    </h1>
+                  </div>
+                  <div className="sub-region">
+                    <h1>
+                      Sub Region:
+                      <span>{countrie[0].subregion}</span>
+                    </h1>
+                  </div>
+                  <div className="capital">
+                    <h1>
+                      Capital:
+                      <span>{countrie[0].capital}</span>
+                    </h1>
+                  </div>
+                  <div className="top-level-domain">
+                    <h1>
+                      Top Level Domain:
+                      <span>{countrie[0].topLevelDomain}</span>
+                    </h1>
+                  </div>
+                  <div className="currencies">
+                    <h1>
+                      Currencies:
+                      <span>{countrie[0].currencies[0].code}</span>
+                    </h1>
+                  </div>
+                  <div className="languages">
+                    <h1>
+                      Languages:
+                      <span>{languages}</span>
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </CountryDetail>
           <Countries className="countries-conteiner">
             {countries.map((value) => value)}
           </Countries>
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -21,10 +21,18 @@ export const Section = styled.section`
   }
 `;
 export const Header = styled.header`
+  position: relative;
+  box-shadow: 0px 5px 3px hsl(210, 24%, 13%);
   ${displayFlex('space-between', 'row', 'center')}
   width: 100%;
-  padding: 1.5rem 7rem;
+  padding: 2rem 7rem;
   background-color: ${color.DarkBlue};
+  transition: all 0.5s ease-in-out;
+  z-index: 3;
+  &.open-details {
+    position: fixed;
+    top: 0px;
+  }
   .header-component-one {
     font-size: 2rem;
     color: ${color.FontColorWhite};
@@ -173,5 +181,99 @@ export const Countries = styled.div`
   }
   @media (max-width: 360px) {
     padding: 1.5rem 2rem;
+  }
+`;
+export const CountryDetail = styled.article`
+  position: fixed;
+  overflow: hidden;
+  top: 0px;
+  width: 100%;
+  height: 0vh;
+  padding: 0rem 5rem;
+  background-color: ${color.VeryDarkBlue};
+  z-index: 2;
+  &.open-details {
+    height: 100vh;
+  }
+  .back-button-conteiner {
+    margin: 11rem 0 5rem 0;
+    button {
+      ${displayFlex('center', 'row', 'center')}
+      width: 12rem;
+      height: 4rem;
+      font-size: 1.7rem;
+      border-radius: 5px;
+      box-shadow: 0px 0px 10px hsl(210, 24%, 13%);
+      color: ${color.FontColorWhite};
+      background-color: ${color.DarkBlue};
+      .back-arrow {
+        margin-right: 1rem;
+        color: ${color.FontColorWhite};
+      }
+    }
+  }
+  .conteiner-coutrie-details {
+    ${displayFlex('start', 'row', 'start')}
+    color: ${color.FontColorWhite};
+    .countrie-flag {
+      margin-right: 5rem;
+      img {
+        width: 400px;
+        height: 250px;
+      }
+    }
+    .countrie-details {
+      padding: 2rem;
+      .common-name {
+        font-size: 2.7rem;
+      }
+      .grid-details {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-areas:
+          'content1 content6'
+          'content2 content7'
+          'content3 content8'
+          'content4 conrent4'
+          'content5 content5';
+        width: 900px;
+        margin-top: 2rem;
+        div {
+          margin-top: 0.5rem;
+          h1 {
+            font-size: 1.6rem;
+            color: ${color.FontColorVaryLigthGray};
+            span {
+              margin-left: 1rem;
+              color: ${color.FontColorGray};
+            }
+          }
+        }
+        div:nth-child(1) {
+          grid-area: content1;
+        }
+        div:nth-child(2) {
+          grid-area: content2;
+        }
+        div:nth-child(3) {
+          grid-area: content3;
+        }
+        div:nth-child(4) {
+          grid-area: content4;
+        }
+        div:nth-child(5) {
+          grid-area: content5;
+        }
+        div:nth-child(6) {
+          grid-area: content6;
+        }
+        div:nth-child(7) {
+          grid-area: content7;
+        }
+        div:nth-child(8) {
+          grid-area: content8;
+        }
+      }
+    }
   }
 `;
